3df6cc5760f771e5b03977c9879b8d6340083708
Darren K. Maczka <dmaczka@vt.edu>
2012-03-13 16:43:47 -0400

    Updated python template to use fileinput module

--- filter_template.py
+++ filter_template.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python2
-from sys import stdout
+import fileinput
+from sys import stderr,stdout
 
-def read_buffer_lines(buffer):
-	linecount=0
-
-	line = buffer.readline()
-	while(line):
-		stdout.write("line: %s" % line)
-		linecount=linecount+1
-		line = buffer.readline()
-
-	stdout.write("line count: %d\n" % linecount)	
-
-if __name__ == '__main__':
-	from sys import argv,stderr,stdin
-
-	if len(argv) > 1:
-		filename=argv[1]
-		stderr.write("Reading from file: %s" % filename)
-		with open(filename, 'r') as buffer:
-			read_buffer_lines(buffer)
-	else:
-		stderr.write("Reading lines from stdin")
-		with stdin as buffer:
-			read_buffer_lines(buffer)
-
-
+for line in fileinput.input():
+	if fileinput.isfirstline():
+		stderr.write("Reading from %s\n" % fileinput.filename())
+	stdout.write("line: %s" % line)
