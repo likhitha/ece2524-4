ca99f591981c5dd6fdf69c97f2055d48617e6853
Ramona Flowers <rflowers@hazyblue.net>
2012-03-14 09:45:53 -0400

    updated match_action to move algorithm into a function

--- match_action.py
+++ match_action.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
 import re
-from sys import stdin,stdout,stderr
 
-total=0
-for line in stdin:
-    if re.match('##',line):
-        stdout.write(line)
-        numbers = re.findall(r'\(([-+][0-9]+)\)',line)
-        if numbers:
-            for number in numbers:
-                total += float(number)
-
-stderr.write('Total: %f\n' % total)
+def comment_parse(data):
+    comments=""
+    total=0
+    for line in data:
+        if re.match('##',line):
+            comments += line
+            numbers = re.findall(r'\(([-+][0-9]+)\)',line)
+            if numbers:
+                for number in numbers:
+                    total += float(number)
+    return (total, comments)
+  
+if __name__=='__main__':
+    from sys import stdin,stdout,stderr
+    
+    (total, comments) = comment_parse(stdin)
+    stderr.write(comments)
+    stderr.write('Total: %f\n' % total)
                 
