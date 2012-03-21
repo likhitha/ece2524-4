3ea719983b0a8e2341d0350849c6ebe30ad6df8c
Darren Maczka <dmaczka@vt.edu>
2012-03-21 09:25:33 -0400

    Fixed typo in args.cpp

--- week10/args.cpp
+++ week10/args.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main (int argc, const char* argv[])
 {
-  for (unsinged int i=0; i<argc; i++)
+  for (int i=0; i<argc; i++)
     cout << "argv[" << i << "]=" << argv[i] << endl;
 
   return EXIT_SUCCESS;
