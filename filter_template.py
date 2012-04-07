d37c57780d1860b4813e6a97151c554463148e9d
unknown <csbarcode@gmail.com>
2012-04-07 14:44:13 -0400

    removing things

--- filter_template.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python2
-import fileinput
-from sys import stderr,stdout
-
-for line in fileinput.input():
-	if fileinput.isfirstline():
-		stderr.write("\033[1;31mReading from %s\033[0m\n" % fileinput.filename())
-	stdout.write("line: %s" % line)
--- match_import.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import match_action
-from sys import stdin,stdout
-
-(total, comments) = match_action.comment_parse(stdin)
-
-stdout.write(comments)
-stdout.write("Total: %d\n" % total)
