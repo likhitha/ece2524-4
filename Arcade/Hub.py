f41380e3028dc513a1cc07d53b0b3e29941fc0c2
unknown <csbarcode@gmail.com>
2012-04-07 14:41:53 -0400

    setting up for project

---
+++ Arcade/Hub.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from sys import stdin, stdout
+
+def help_msg():
+    print(argv[0], ' arg1 arg2 arg3')
+    print('Options:')
+    print('--help : Display this help text')
+    print('--del arg : Delete 
